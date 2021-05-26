35887b6c2070e3a62481d6f84ef0f72d559b58c1
aluno <caio.donat@es.estudante.senai.br>
2021-05-26 17:13:29 -0300

    case Exercicio_05

--- src/ExerciciosFuncoes5/Exercicio_04.java
+++ src/ExerciciosFuncoes5/Exercicio_04.java
@@ -1,10 +1,9 @@
 package ExerciciosFuncoes5;
+
 /*
 1.solicito um numero real
 2.chamo o metodo com "numero real" como parametro
-3.faço a comparacao ("numero real" > 0 = positivo, "numero real" < 0 = negativo, "numero real" == 0 = 0)
-4.retorno 'p' (positivo), 'n' (negativo) ou 'z' (numero real = 0)
-5.devolta do bloco 'main' faço a comparação do char e de acordo com o valor imprimo a mensagem correta.
+3.faço (dentro do metodo) a comparacao ("numero real" > 0 = positivo | "numero real" < 0 = negativo | "numero real" == 0 = 0)
  */
 
 import java.util.Scanner;
@@ -12,34 +11,19 @@ import java.util.Scanner;
 public class Exercicio_04 {
 
     public static void main(String[] args) {
-        System.out.println("Digite um numero");
+        System.out.println("Digite um numero");//1 - Começo
         Scanner myScanner = new Scanner(System.in);
-        float numeroDigitado = myScanner.nextFloat();
-        char resultado = positivoOuNegativo(numeroDigitado);
-
-        switch (resultado){
-            case 'p': {
-                System.out.println("O numero é positivo");
-                break;
-            }
-            case 'n': {
-                System.out.println("O numero é negativo");
-                break;
-            }
-            case 'z': {
-                System.out.println("O numero é igual há zero");
-                break;
-            }
-        }
+        float numeroDigitado = myScanner.nextFloat();//1 - Fim
+        positivoOuNegativo(numeroDigitado);//2
     }
 
-    private static char positivoOuNegativo(float numeroDigitado) {
-        if (numeroDigitado > 0){
-            return 'p';
+    private static void positivoOuNegativo(float numeroDigitado) {
+        if (numeroDigitado > 0){//3 -Começo
+            System.out.println("Numero Positivo");
         }else if (numeroDigitado < 0){
-            return 'n';
+            System.out.println("Numero Negativo");
         }else {
-            return 'z';
-        }
+            System.out.println("Numero = 0");
+        }//3 - Fim
     }
 }
