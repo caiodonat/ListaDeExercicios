d2d1c3685fe9d4addd499833366e312b4ad580e6
aluno <caio.donat@es.estudante.senai.br>
2021-05-28 15:40:36 -0300

    Exercicio_06

--- src/ExerciciosFuncoes5/Exercicio_06.java
+++ src/ExerciciosFuncoes5/Exercicio_06.java
@@ -5,17 +5,28 @@ import java.util.Scanner;
 public class Exercicio_06 {
     public static void main(String[] args) {
 
-        System.out.println("14:30");
+        System.out.println("Digite um horaio, no formato 24 horas (14:30, 12:25, 02:05)");
         Scanner myScanner = new Scanner(System.in);
         String horasDigitada = myScanner.next();
+        char apPmMain = ' ';
+        horasDigitadasSeArreyInt(horasDigitada, apPmMain);
+
+        System.out.println(apPmMain);
+    }
+    static int[] horasDigitadasSeArreyInt(String horasDigitada, char letraRetornada){
         String horasDigitadasSeArrey[] = horasDigitada.split(":");
         int[] horasDigitadasSeArreyInt = new int[horasDigitadasSeArrey.length];
 
         for (int i=0; i<horasDigitadasSeArreyInt.length; i++){
             horasDigitadasSeArreyInt[i] = Integer.parseInt(horasDigitadasSeArrey[i]);
         }
-        for (int mostrarNaTela : horasDigitadasSeArreyInt){
-            System.out.println(mostrarNaTela);
+        if (horasDigitadasSeArreyInt[0] > 12){
+            horasDigitadasSeArreyInt[0] =- 12;
+            letraRetornada = 'a';
+        }else {
+            letraRetornada = 'p';
         }
+        char amPm = 'p';
+        return horasDigitadasSeArreyInt;
     }
 }
