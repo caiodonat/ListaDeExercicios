0aeaad50a9d4b747a5c95a59b3c33db2de254058
aluno <caio.donat@es.estudante.senai.br>
2021-05-28 14:03:11 -0300

    Exercicio_06

--- src/ExerciciosFuncoes5/Exercicio_06.java
+++ src/ExerciciosFuncoes5/Exercicio_06.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 
 public class Exercicio_06 {
     public static void main(String[] args) {
+
         System.out.println("14:30");
         Scanner myScanner = new Scanner(System.in);
         String horasDigitada = myScanner.next();
-        String horasPrimeiraParte = horasDigitada.substring(0, 2);
-        String horasPrimeiraParteInt = (int) horasPrimeiraParte;
-        System.out.println(horasPrimeiraParteInt);
+        String horasDigitadasSeArrey[] = horasDigitada.split(":");
+        int[] horasDigitadasSeArreyInt = new int[horasDigitadasSeArrey.length];
+
+        for (int i=0; i<horasDigitadasSeArreyInt.length; i++){
+            horasDigitadasSeArreyInt[i] = Integer.parseInt(horasDigitadasSeArrey[i]);
+        }
+        for (int mostrarNaTela : horasDigitadasSeArreyInt){
+            System.out.println(mostrarNaTela);
+        }
     }
 }
