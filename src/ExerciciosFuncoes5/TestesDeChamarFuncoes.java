98e0af67758448df5d841ba777b64fef042e27ad
Caio_Henrique_Donat <82171738+4361696F@users.noreply.github.com>
2021-05-31 15:37:13 -0300

    Exercicio_09 Finalizado

--- src/ExerciciosFuncoes5/TestesDeChamarFuncoes.java
+++ src/ExerciciosFuncoes5/TestesDeChamarFuncoes.java
@@ -1,8 +1,8 @@
-package ExerciciosFuncoes5.Exercicio_07;
+package ExerciciosFuncoes5;
 
 import ExerciciosFuncoes5.Exercicio_01;
 
-public class Exercicio_07_exibicaoDeFimDoDia {
+public class TestesDeChamarFuncoes {
     public static void main(String[] args) {
         Exercicio_01 nomeDaVariavel = new Exercicio_01();
         nomeDaVariavel.repeticao(3);
